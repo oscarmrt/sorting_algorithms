1495bb9df9dfee2dac927c73bbe5b6384b8193d0
oscarmrt <oscarmrt@hotmail.com>
2020-02-05 15:49:21 +0000

    modified file 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -14,7 +14,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *tmp = NULL;
 
 
-	if (*list == NULL || (*list)->next == NULL)
+	if (*list == NULL || (*list)->next == NULL || list == NULL)
 		return;
 
 	nd = (*list)->next;
