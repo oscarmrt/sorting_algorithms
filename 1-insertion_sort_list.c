80c1e36294d4f8a1b4278eff0c9e55ecf195d851
oscarmrt <oscarmrt@hotmail.com>
2020-02-05 15:51:45 +0000

    modified file 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -14,34 +14,36 @@ void insertion_sort_list(listint_t **list)
 	listint_t *tmp = NULL;
 
 
-	if (*list == NULL || (*list)->next == NULL || list == NULL)
+	if (*list == NULL || (*list)->next == NULL)
 		return;
-
-	nd = (*list)->next;
-	while (nd != NULL)
+	else
 	{
-		if (nd->n < nd->prev->n)
+		nd = (*list)->next;
+		while (nd != NULL)
 		{
-			tmp = nd;
-			swp = tmp->prev;
-			nd = nd->next;
-			while (swp != NULL && tmp->n < swp->n)
+			if (nd->n < nd->prev->n)
 			{
-				if (swp->prev != NULL)
-					swp->prev->next = tmp;
-				tmp->prev = swp->prev;
-				if (tmp->next != NULL)
-					tmp->next->prev = swp;
-				swp->next = tmp->next;
-				swp->prev = tmp;
-				tmp->next = swp;
+				tmp = nd;
 				swp = tmp->prev;
-				if ((*list)->prev != NULL)
-					*list = (*list)->prev;
-				print_list(*list);
+				nd = nd->next;
+				while (swp != NULL && tmp->n < swp->n)
+				{
+					if (swp->prev != NULL)
+						swp->prev->next = tmp;
+					tmp->prev = swp->prev;
+					if (tmp->next != NULL)
+						tmp->next->prev = swp;
+					swp->next = tmp->next;
+					swp->prev = tmp;
+					tmp->next = swp;
+					swp = tmp->prev;
+					if ((*list)->prev != NULL)
+						*list = (*list)->prev;
+					print_list(*list);
+				}
 			}
+			else
+				nd = nd->next;
 		}
-		else
-			nd = nd->next;
 	}
 }
